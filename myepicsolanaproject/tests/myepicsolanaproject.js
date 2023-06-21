cbbd1b40b9494abbe0ce6a147bbbf0657f8c493b
Michle99 <sammiyonyx@gmail.com>
2023-06-21 18:29:40 -0400

    Added myepicsolanaproject backend.

---
+++ myepicsolanaproject/tests/myepicsolanaproject.js
@@ -0,0 +1,75 @@
+const anchor = require("@coral-xyz/anchor");
+const { program } = require("@coral-xyz/anchor/dist/cjs/native/system");
+
+// describe("myepicsolanaproject", () => {
+//   // Configure the client to use the local cluster.
+//   anchor.setProvider(anchor.AnchorProvider.env());
+
+//   it("Is initialized!", async () => {
+//     // Add your test here.
+//     const program = anchor.workspace.Myepicsolanaproject;
+//     const tx = await program.methods.initialize().rpc();
+//     console.log("Your transaction signature", tx);
+//   });
+// });
+
+// Need the system program, will talk about this soon.
+const { SystemProgram } = anchor.web3;
+
+const main = async() => {
+  console.log("🚀 Starting test...")
+
+  // Create and set the provider. We set it before but we needed to update it, so that it can communicate with our frontend!
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+
+  const program = anchor.workspace.Myepicsolanaproject;
+	
+  // Create an account keypair for our program to use.
+  const baseAccount = anchor.web3.Keypair.generate();
+
+  // Call start_stuff_off, pass it the params it needs!
+  let tx = await program.rpc.startStuffOff({
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey,
+      systemProgram: SystemProgram.programId,
+    },
+    signers: [baseAccount],
+  });
+
+  console.log("📝 Your transaction signature", tx);
+
+  // Fetch data from the account.
+  let account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log('👀 GIF Count', account.totalGifs.toString())
+
+  // Call add_gif
+  await program.rpc.addGif("insert_a_giphy_link_here", {
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey,
+    },
+  });
+
+  // Fetch data from the account.
+  account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log('👀 GIF Count', account.totalGifs.toString())
+
+ // Access gif_list on the account!
+ console.log('👀 GIF List', account.gifList)
+}
+
+
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+runMain();
